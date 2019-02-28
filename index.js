46297b9f3bc598cc26dcdad17274c2df4b177d95
ColinTree <502470184@qq.com>
2019-02-28 21:34:00 +0800

    Added web interface entry

---
+++ index.js
@@ -0,0 +1,20 @@
+let http = require("http");
+
+const PORT = 80;
+
+start();
+
+function start() {
+  let server = http.createServer(function(request, response) {
+    response.writeHead(200);
+    response.write("Welcome to bot.colintree.cn");
+    response.write("<ol>");
+    response.write("<li><a href=\"http://bot.colintree.cn:6474\">Webhook: port 6474</a></li>");
+    response.write("<li><a href=\"http://bot.colintree.cn:8048\">Extension builder: port 8048</a></li>");
+    response.write("<li><a href=\"http://bot.colintree.cn:8049\">Extension builder dev: port 8049</a></li>");
+    response.write("</ol>");
+    response.end();
+  });
+  console.log("listening port at: " + PORT);
+  server.listen(PORT);
+}
