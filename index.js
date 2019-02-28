392bbd8f734cf73c65bada08b203ad80a6ea2d36
ColinTree <502470184@qq.com>
2019-02-28 21:43:13 +0800

    Added contype to ensure user see a webpage

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ start();
 
 function start() {
   let server = http.createServer(function(request, response) {
-    response.writeHead(200);
+    response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
     response.write("Welcome to bot.colintree.cn");
     response.write("<ol>");
     response.write("<li><a href=\"http://bot.colintree.cn:6474\">Webhook: port 6474</a></li>");
